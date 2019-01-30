3a71a724284d9a57dc425b601b52eb89d000d728
Daniel Shimield <dxshim90@gmail.com>
2019-01-30 11:19:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,12 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each { |word| }
+  array.each { |word| 
   if word > 4
     return true
   else
     return false
+  }
   end
 end
 
