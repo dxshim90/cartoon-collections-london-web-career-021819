7d10672b712f60ce852164b3c66234528e0aef7d
Daniel Shimield <dxshim90@gmail.com>
2019-01-30 11:23:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each { |word| word > 4 true : false }
+  array.each { |word| word > 4 : false }
 end
 
 def find_the_cheese# code an argument here
