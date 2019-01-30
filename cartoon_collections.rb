7abc7af869291f68b37d0370cbc9dd31ece3f243
Daniel Shimield <dxshim90@gmail.com>
2019-01-30 11:19:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,13 @@ def summon_captain_planet(array)
   array.map { |word| word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each { |word| }
+  if word > 4
+    return true
+  else
+    return false
+  end
 end
 
 def find_the_cheese# code an argument here
