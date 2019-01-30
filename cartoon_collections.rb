582c7028abb2b53b9fa8370749f23c8acbb64acb
Daniel Shimield <dxshim90@gmail.com>
2019-01-30 11:08:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |word| word.Upcase + "!"}
+  array.map { |word| word.upcase! + "!"}
 end
 
 def long_planeteer_calls# code an argument here
