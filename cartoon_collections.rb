efd241282782985e8f5ce6f195756534c5d76390
Daniel Shimield <dxshim90@gmail.com>
2019-01-30 11:31:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,9 @@ end
 
 def long_planeteer_calls(array)
   array.each { |word| 
-  if word.length <= 4
+  if word.length < 4
     return false
-  else
+  elsif word.length > 4
     return true
   end }
 end
